38a335b383bb045505aa93ddc37314a829e5298b
aaronmyatt <aaronmyatt@gmail.com>
2021-02-08 00:27:30 +0800

    additional livestock form test

--- tests/Feature/LivestockForm.php
+++ tests/Feature/LivestockForm.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Livewire\Livewire;
 
 it('redirects to livestock page', function () {
+    
     $this->actingAs(User::factory()->create());
 
     Livewire::test(Form::class)
@@ -16,4 +17,19 @@ it('redirects to livestock page', function () {
         ->set('livestock.gender', 'male')
         ->call('save')
         ->assertRedirect();
+});
+
+it('saves one new livestock row', function () {
+    $this->actingAs(User::factory()->create());
+
+    Livewire::test(Form::class)
+        ->set('livestock.site_id', 1)
+        ->set('livestock.tank_id', 1)
+        ->set('livestock.body_weight_grams', 15)
+        ->set('livestock.number_of_pieces', 15)
+        ->set('livestock.gender', 'male')
+        ->call('save');
+
+    Ray(Livestock::count())->red();
+    // $this->assertTrue(Livestock::count() === 1);
 });
