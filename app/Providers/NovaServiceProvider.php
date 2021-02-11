84b080735a0d1b5c8f30832a698cc5f61d0ffa76
aaronmyatt <aaronmyatt@gmail.com>
2021-02-11 17:48:26 +0800

    uses explode properly -.-

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {            
-            return in_array($user->email, explode(env('NOVA_USERS'), ','));
+            return in_array($user->email, explode(',', env('NOVA_USERS')));
         });
     }
 
