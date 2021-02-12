48c60a1bdcfeaf2ac151754246ad2e5fdbe45202
aaronmyatt <aaronmyatt@gmail.com>
2021-02-12 19:22:55 +0800

    adds date sequence

--- database/seeders/LivestockSeeder.php
+++ database/seeders/LivestockSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class LivestockSeeder extends Seeder
@@ -13,7 +14,11 @@ class LivestockSeeder extends Seeder
      */
     public function run()
     {   
-        $nopSequence = array_map(function($n){ return [ 'number_of_pieces' => $n ]; } ,range(500,0,3));
+        $nopSequence = array_map(function($n){ return [ 'number_of_pieces' => $n ]; }, range(500,0,3));
+        Ray($nopSequence);
+        $nopSequence = array_map(function($nop, $n){ 
+            return array_merge($nop ? $nop : [], [ 'updated_at' => Carbon::now()->subYear(2)->addWeek($n) ]); 
+        }, $nopSequence, range(0, count($nopSequence)));
         \App\Models\Livestock::factory(100)
             ->sequence(...$nopSequence)
             ->create();
